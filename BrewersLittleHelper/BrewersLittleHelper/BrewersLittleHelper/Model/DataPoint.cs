0b27d7e1039c8b7dacef30cd4f7f1bdea9e0859f
PlanetExp <captain.hydd@gmail.com>
2016-05-19 14:03:27 +1000

    starting to work on a graph

---
+++ BrewersLittleHelper/BrewersLittleHelper/BrewersLittleHelper/Model/DataPoint.cs
@@ -0,0 +1,17 @@
+﻿
+// Place appropriately, this is the data point model for use in the charts
+
+using System;
+
+namespace BrewersLittleHelper
+{
+	public class DataPoint
+	{
+		// x axis
+		public string Date { get; set; }
+
+		// y axis
+		public int value { get; set; }
+	}
+}
+
---
+++ BrewersLittleHelper/BrewersLittleHelper/BrewersLittleHelper/ViewModel/ChartViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace BrewersLittleHelper
+{
+	public class ChartViewModel
+	{
+
+		public ObservableCollection<DataPoint> ChartData { get; set; }
+
+		public ChartViewModel ()
+		{
+			ChartData = new ObservableCollection<DataPoint> ();
+		}
+	}
+}
+
